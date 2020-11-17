163cf112008775fe673ab2f10a307d0c9861a902
Cecile <cecilems@msn.com>
2020-11-17 14:47:05 +0100

    Ajout du menu de tête

--- header.php
+++ header.php
@@ -12,4 +12,11 @@
     <header>
         <h1><a><?php bloginfo('name'); ?></a></h1>
         <h2><?php bloginfo('description'); ?></h2>
+        <nav>
+        <?php
+            wp_nav_menu ( array (
+            'theme_location' => 'header-menu'
+            ) ); 
+            ?>
+        </nav>
     </header>
