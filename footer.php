b0675b1000bdb81f9fee4e975d2fab7e5ca3636d
Cecile <cecilems@msn.com>
2020-11-17 14:56:30 +0100

    ajout de menus + footer

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 <footer>
     <?php bloginfo('name'); ?> est propulsé par <a
     href="https://wordpress.org">WordPress</a>.
+        <nav><?php
+            wp_nav_menu ( array (
+            'theme_location' => 'footer-menu'
+    ) ); ?></nav>
         </footer>
         </div>
         <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
 
-//ajouter une nouvelle zone de menu à mon thème
-function register_my_menu() {
-register_nav_menu('header-menu',__( 'Menu De Tete' ));
-}
-add_action( 'init', 'register_my_menu' );
+//ajouter une nouvelle zone de menus à mon thème
+
+
+function register_my_menu(){
+    register_nav_menus( array(
+    'header-menu' => __( 'Menu De Tete'),
+    'footer-menu' => __( 'Menu De Pied'),
+    ) );
+    }
+    add_action( 'init', 'register_my_menu', 0 );
