5266aba0e79ceec6461be091bc85792c302082f2
Cecile <cecilems@msn.com>
2020-11-26 14:23:18 +0100

    ajout du champ Recette et de thumbnail

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+//ajout du support des vignettes
+add_theme_support( 'post-thumbnails' );
+
+
 
 //ajouter une nouvelle zone de menus à mon thème
 
@@ -10,3 +14,42 @@ function register_my_menu(){
     ) );
     }
     add_action( 'init', 'register_my_menu', 0 );
+
+    // ajout du custom post
+
+function init_my_custom()
+    {
+     register_post_type(
+     'recette',
+     array(
+     'label' => 'recettes',
+     'labels' => array(
+     'name' => 'Recettes',
+     'singular_name' => 'Recette',
+     'all_items' => 'Toutes les recettes',
+     'add_new_item' => 'Ajouter une recette',
+     'edit_item' => 'Éditer la recette',
+     'new_item' => 'Nouvelle recette',
+     'view_item' => 'Voir la recette',
+     'search_items' => 'Rechercher parmi les recettes',
+     'not_found' => 'Pas de recette trouvée',
+     'not_found_in_trash'=> 'Pas de recette dans la corbeille'
+     ),
+     'public' => true,
+     'capability_type' => 'post',
+     'supports' => array(
+     'title',
+     'editor',
+     'thumbnail'
+     ),
+     'has_archive' => true
+     )
+     );
+    }
+    add_action('init', 'init_my_custom');
+
+
+
+
+
+    ?>
