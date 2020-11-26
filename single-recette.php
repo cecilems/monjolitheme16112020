6f71dce03e35e7ce7b3f4cb5ca9d0dcbb809cd98
Cecile <cecilems@msn.com>
2020-11-26 15:47:40 +0100

    ajout des aliments dans la recette

--- single-recette.php
+++ single-recette.php
@@ -1,16 +1,37 @@
 <?php get_header(); ?>
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <article class="recette">
-            <?php the_post_thumbnail ( 'large' );
-            ?>
-            <h1 class="title">
-            <?php the_title(); ?>
-            </h1>
-            <div class="content">
-            <?php the_content (); ?>
-            </div>
-            </article>
-        <?php endwhile; ?>
-    <?php endif; ?>
+<?php if (have_posts()) : ?>
+   <?php while (have_posts()) : the_post(); ?>
+       <article class="recette">
+           <?php the_post_thumbnail( 'large' ); ?>
+           <h1 class="title">
+           <?php the_title(); ?>
+           </h1>
+           <div class="content">
+               <?php the_content(); ?>
+
+               <?php // Affichage des aliments en lien avec la recette
+                $featured_posts = get_field('ingredient');
+                if( $featured_posts ): ?>
+                    <ul>
+                    <?php foreach( $featured_posts as $post ): 
+
+                        // Setup this post for WP functions (variable must be named $post).
+                        setup_postdata($post); ?>
+                        <li>
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            <span>Glucide: <?php the_field( 'sucre' ); ?></span>
+                            <span>Graisse: <?php the_field( 'graisse' ); ?></span>
+                            <span>Protéine: <?php the_field( 'proteine' ); ?></span>
+                        </li>
+                    <?php endforeach; ?>
+                    </ul>
+                    <?php 
+                    // Reset the global post object so that the rest of the page works correctly.
+                    wp_reset_postdata(); ?>
+                <?php endif; ?>
+
+           </div>
+       </article>
+   <?php endwhile; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
